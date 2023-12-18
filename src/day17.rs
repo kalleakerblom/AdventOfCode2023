1e4cfa2734bf5c92ba4cdd0be2c409fa6cceb99a
kalleakerblom <kalleakerblom@gmail.com>
2023-12-18 20:26:01 +0100

    refactor + speedup day17

--- src/day17.rs
+++ src/day17.rs
@@ -1,4 +1,7 @@
-use std::collections::{hash_map::Entry, HashMap};
+use std::{
+    cmp::Reverse,
+    collections::{hash_map::Entry, BinaryHeap, HashMap},
+};
 
 struct Map {
     tiles: Vec<Vec<u32>>,
@@ -31,107 +34,31 @@ impl Map {
 }
 type Pos = (i16, i16);
 
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
 enum Dir {
     Up,
     Down,
     Left,
     Right,
 }
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy, PartialOrd, Ord)]
 struct State {
     dir: Dir,
     steps: u8,
 }
-//(pos, cost, state)
-fn next_steps(map: &Map, cost: u32, (x, y): Pos, steps: u8, dir: Dir) -> Vec<(Pos, u32, State)> {
-    let mut next = vec![];
-    let up = (x, y - 1);
-    let down = (x, y + 1);
-    let right = (x + 1, y);
-    let left = (x - 1, y);
-    let up_cost = map.cost(up).map(|c| c + cost);
-    let down_cost = map.cost(down).map(|c| c + cost);
-    let left_cost = map.cost(left).map(|c| c + cost);
-    let right_cost = map.cost(right).map(|c| c + cost);
-    #[rustfmt::skip]
-    let next_up = up_cost.map(|c| {(up, c, State {dir: Dir::Up, steps: 1})});
-    #[rustfmt::skip]
-    let next_down = down_cost.map(|c| {(down, c, State {dir: Dir::Down, steps: 1})});
-    #[rustfmt::skip]
-    let next_left = left_cost.map(|c| {(left, c, State {dir: Dir::Left, steps: 1})});
-    #[rustfmt::skip]
-    let next_right = right_cost.map(|c| {(right, c, State {dir: Dir::Right, steps: 1}) });
 
-    match dir {
-        Dir::Up => {
-            if steps < 3 && up_cost.is_some() {
-                next.push((
-                    up,
-                    up_cost.unwrap(),
-                    State {
-                        dir: Dir::Up,
-                        steps: steps + 1,
-                    },
-                ));
-            }
-            next.extend(next_right);
-            next.extend(next_left);
-        }
-        Dir::Down => {
-            if steps < 3 && down_cost.is_some() {
-                next.push((
-                    down,
-                    down_cost.unwrap(),
-                    State {
-                        dir: Dir::Down,
-                        steps: steps + 1,
-                    },
-                ));
-            }
-            next.extend(next_right);
-            next.extend(next_left);
-        }
-        Dir::Left => {
-            if steps < 3 && left_cost.is_some() {
-                next.push((
-                    left,
-                    left_cost.unwrap(),
-                    State {
-                        dir: Dir::Left,
-                        steps: steps + 1,
-                    },
-                ));
-            }
-            next.extend(next_up);
-            next.extend(next_down);
-        }
-        Dir::Right => {
-            if steps < 3 && right_cost.is_some() {
-                next.push((
-                    right,
-                    right_cost.unwrap(),
-                    State {
-                        dir: Dir::Right,
-                        steps: steps + 1,
-                    },
-                ));
-            }
-            next.extend(next_up);
-            next.extend(next_down);
-        }
-    }
-    next
-}
-
-fn next_steps_part2(
+fn next_steps(
     map: &Map,
     cost: u32,
     (x, y): Pos,
-    steps: u8,
-    dir: Dir,
-) -> Vec<(Pos, u32, State)> {
-    let mut next = vec![];
+    State { dir, steps }: State,
+    to_visit: &mut BinaryHeap<(Reverse<u32>, Pos, State)>,
+    part: Part,
+) {
+    let (min_steps, max_steps) = match part {
+        Part::One => (0, 3),
+        Part::Two => (4, 10),
+    };
     let up = (x, y - 1);
     let down = (x, y + 1);
     let right = (x + 1, y);
@@ -141,95 +68,79 @@ fn next_steps_part2(
     let left_cost = map.cost(left).map(|c| c + cost);
     let right_cost = map.cost(right).map(|c| c + cost);
     #[rustfmt::skip]
-    let next_up = up_cost.map(|c| {(up, c, State {dir: Dir::Up, steps: 1})});
+    let next_up = up_cost.map(|c| {( Reverse(c), up, State {dir: Dir::Up, steps: 1})});
     #[rustfmt::skip]
-    let next_down = down_cost.map(|c| {(down, c, State {dir: Dir::Down, steps: 1})});
+    let next_down = down_cost.map(|c| {( Reverse(c), down, State {dir: Dir::Down, steps: 1})});
     #[rustfmt::skip]
-    let next_left = left_cost.map(|c| {(left, c, State {dir: Dir::Left, steps: 1})});
+    let next_left = left_cost.map(|c| {(Reverse(c), left,  State {dir: Dir::Left, steps: 1})});
     #[rustfmt::skip]
-    let next_right = right_cost.map(|c| {(right, c, State {dir: Dir::Right, steps: 1}) });
+    let next_right = right_cost.map(|c| {(Reverse(c), right,  State {dir: Dir::Right, steps: 1}) });
 
     match dir {
         Dir::Up => {
-            if steps < 4 && up_cost.is_none() {
-                return vec![];
+            if steps < min_steps && up_cost.is_none() {
+                return;
             }
-            if steps < 10 && up_cost.is_some() {
-                next.push((
-                    up,
-                    up_cost.unwrap(),
-                    State {
-                        dir: Dir::Up,
-                        steps: steps + 1,
-                    },
-                ));
+            if steps < max_steps {
+                if let Some(mut n) = next_up {
+                    n.2.steps = 1 + steps;
+                    to_visit.push(n);
+                }
             }
-            if steps >= 4 {
-                next.extend(next_right);
-                next.extend(next_left);
+            if steps >= min_steps {
+                to_visit.extend(next_right);
+                to_visit.extend(next_left);
             }
         }
         Dir::Down => {
-            if steps < 4 && down_cost.is_none() {
-                return vec![];
+            if steps < min_steps && down_cost.is_none() {
+                return;
             }
-            if steps < 10 && down_cost.is_some() {
-                next.push((
-                    down,
-                    down_cost.unwrap(),
-                    State {
-                        dir: Dir::Down,
-                        steps: steps + 1,
-                    },
-                ));
+            if steps < max_steps {
+                if let Some(mut n) = next_down {
+                    n.2.steps = 1 + steps;
+                    to_visit.push(n);
+                }
             }
-            if steps >= 4 {
-                next.extend(next_right);
-                next.extend(next_left);
+            if steps >= min_steps {
+                to_visit.extend(next_right);
+                to_visit.extend(next_left);
             }
         }
         Dir::Left => {
-            if steps < 4 && left_cost.is_none() {
-                return vec![];
+            if steps < min_steps && left_cost.is_none() {
+                return;
             }
-            if steps < 10 && left_cost.is_some() {
-                next.push((
-                    left,
-                    left_cost.unwrap(),
-                    State {
-                        dir: Dir::Left,
-                        steps: steps + 1,
-                    },
-                ));
+            if steps < max_steps {
+                if let Some(mut n) = next_left {
+                    n.2.steps = 1 + steps;
+                    to_visit.push(n);
+                }
             }
-            if steps >= 4 {
-                next.extend(next_up);
-                next.extend(next_down);
+            if steps >= min_steps {
+                to_visit.extend(next_up);
+                to_visit.extend(next_down);
             }
         }
         Dir::Right => {
-            if steps < 4 && right_cost.is_none() {
-                return vec![];
+            if steps < min_steps && right_cost.is_none() {
+                return;
             }
-            if steps < 10 && right_cost.is_some() {
-                next.push((
-                    right,
-                    right_cost.unwrap(),
-                    State {
-                        dir: Dir::Right,
-                        steps: steps + 1,
-                    },
-                ));
+            if steps < max_steps {
+                if let Some(mut n) = next_right {
+                    n.2.steps = 1 + steps;
+                    to_visit.push(n);
+                }
             }
-            if steps >= 4 {
-                next.extend(next_up);
-                next.extend(next_down);
+            if steps >= min_steps {
+                to_visit.extend(next_up);
+                to_visit.extend(next_down);
             }
         }
     }
-    next
 }
 
+#[derive(Clone, Copy)]
 enum Part {
     One,
     Two,
@@ -237,28 +148,26 @@ enum Part {
 
 fn search(map: Map, part: Part) -> u32 {
     let mut min_cost = HashMap::new();
-    let mut to_visit = vec![
-        (
-            (0, 0),
-            0,
-            State {
-                dir: Dir::Right,
-                steps: 0,
-            },
-        ),
-        (
-            (0, 0),
-            0,
-            State {
-                dir: Dir::Down,
-                steps: 0,
-            },
-        ),
-    ];
+    let mut to_visit_ = BinaryHeap::new();
+    to_visit_.push((
+        Reverse(0),
+        (0, 0),
+        State {
+            dir: Dir::Right,
+            steps: 0,
+        },
+    ));
+    to_visit_.push((
+        Reverse(0),
+        (0, 0),
+        State {
+            dir: Dir::Down,
+            steps: 0,
+        },
+    ));
     let mut best = u32::MAX;
-    const MAX_COST: u32 = 1000;
-    while let Some((pos, cost, state)) = to_visit.pop() {
-        if cost > best || cost > 1000 {
+    while let Some((Reverse(cost), pos, state)) = to_visit_.pop() {
+        if cost > best {
             continue;
         }
         match min_cost.entry((pos, state)) {
@@ -280,16 +189,15 @@ fn search(map: Map, part: Part) -> u32 {
                     best = best.min(cost);
                     continue;
                 }
-                to_visit.extend(next_steps(&map, cost, pos, state.steps, state.dir));
             }
             Part::Two => {
                 if pos == (map.width as i16 - 1, map.height as i16 - 1) && state.steps >= 4 {
                     best = best.min(cost);
                     continue;
                 }
-                to_visit.extend(next_steps_part2(&map, cost, pos, state.steps, state.dir));
             }
         }
+        next_steps(&map, cost, pos, state, &mut to_visit_, part);
     }
     best
 }
