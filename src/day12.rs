bcc7308d589bd35f33843e9ff3a2e20eb94e88d9
kalleakerblom <kalleakerblom@gmail.com>
2023-12-18 20:28:33 +0100

    Refactor day12, WIP still too slow

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,25 @@
-fn check_record(count: usize, instruction: &[char], criteria_groupings: &[usize]) -> u64 {
+fn match_pattern(periods: usize, hashes: usize, instruction: &[char]) -> bool {
+    if periods + hashes > instruction.len() {
+        return false;
+    }
+    let mut iter = instruction.iter();
+    let periods_ok = iter
+        .by_ref()
+        .take(periods)
+        .all(|&ch| ch == '.' || ch == '?');
+    if !periods_ok {
+        return false;
+    }
+    // hashes ok
+    iter.take(hashes).all(|&ch| ch == '#' || ch == '?')
+}
+
+fn count_variations(
+    extra_periods: usize,
+    instruction: &[char],
+    criteria_groupings: &[usize],
+    prev_hash: bool,
+) -> usize {
     if criteria_groupings.is_empty() {
         if instruction.contains(&'#') {
             return 0;
@@ -6,63 +27,36 @@ fn check_record(count: usize, instruction: &[char], criteria_groupings: &[usize]
             return 1;
         }
     }
-
-    if instruction.is_empty() {
-        if criteria_groupings.len() == 1 && count == criteria_groupings[0] {
-            return 1;
-        } else {
-            return 0;
+    let mut sum = 0;
+    for p in 0..=extra_periods {
+        let periods = if prev_hash { p + 1 } else { p };
+        if match_pattern(periods, criteria_groupings[0], instruction) {
+            let pattern_len = periods + criteria_groupings[0];
+            sum += count_variations(
+                extra_periods - p,
+                &instruction[pattern_len..],
+                &criteria_groupings[1..],
+                true,
+            );
         }
     }
-
-    match instruction[0] {
-        '#' => {
-            if count + 1 > criteria_groupings[0] {
-                return 0;
-            }
-            check_record(count + 1, &instruction[1..], criteria_groupings)
-        }
-        '.' => {
-            if count == 0 {
-                check_record(0, &instruction[1..], criteria_groupings)
-            } else if count == criteria_groupings[0] {
-                check_record(0, &instruction[1..], &criteria_groupings[1..])
-            } else {
-                0
-            }
-        }
-        '?' => {
-            let mut res = 0;
-            if count < criteria_groupings[0] {
-                // treat as #
-                res += check_record(count + 1, &instruction[1..], criteria_groupings)
-            }
-            if count == criteria_groupings[0] {
-                // treat as .
-                res += check_record(0, &instruction[1..], &criteria_groupings[1..]);
-            }
-            if count == 0 {
-                // treat as .
-                res += check_record(0, &instruction[1..], criteria_groupings);
-            }
-            res
-        }
-        _ => panic!(),
-    }
+    sum
 }
 
-pub fn part_1(input: &str) -> u64 {
+pub fn part_1(input: &str) -> usize {
     input
         .lines()
         .map(|l| {
             let (instruction, criteria) = l.split_once(' ').unwrap();
             let instruction: Vec<_> = instruction.chars().collect();
             let criteria: Vec<_> = criteria.split(',').map(|n| n.parse().unwrap()).collect();
-            check_record(0, &instruction, &criteria)
+            let min_len: usize = criteria.iter().sum::<usize>() + criteria.len() - 1;
+            let extra_spaces = instruction.len() - min_len;
+            count_variations(extra_spaces, &instruction, &criteria, false)
         })
         .sum()
 }
-pub fn part_2(input: &str) -> u64 {
+pub fn part_2(input: &str) -> usize {
     input
         .lines()
         .map(|l| {
@@ -73,7 +67,9 @@ pub fn part_2(input: &str) -> u64 {
             instruction.pop();
             let criteria: Vec<_> = criteria.split(',').map(|n| n.parse().unwrap()).collect();
             let criteria = criteria.repeat(5);
-            dbg!(check_record(0, &instruction, &criteria))
+            let min_len: usize = criteria.iter().sum::<usize>() + criteria.len() - 1;
+            let extra_spaces = instruction.len() - min_len;
+            count_variations(extra_spaces, &instruction, &criteria, false)
         })
         .sum()
 }
